90a780fb70904b54f9e21cae14bd732695202af1
soma_arc <somaarcr@gmail.com>
2016-03-27 21:15:55 +0900

    Implemente variadic function

--- vec.scm
+++ vec.scm
@@ -4,52 +4,79 @@
 
 (select-module vec)
 
-(define-record-type vec #t #t
-  x y z)
+(define-record-type vec vec3 vec3?
+  (x x x-set!)
+  (y y y-set!)
+  (z z z-set!))
 
-(define (sum v1 v2)
-  (make-vec (+ (vec-x v1) (vec-x v2))
-            (+ (vec-y v1) (vec-y v2))
-            (+ (vec-z v1) (vec-z v2))))
+(define sum
+  (lambda v
+    (reduce (lambda (v1 v2)
+              (vec3 (+ (x v1) (x v2))
+                    (+ (y v1) (y v2))
+                    (+ (z v1) (z v2))))
+            (vec3 0 0 0)
+            v)))
 
-(define (diff v1 v2)
-  (make-vec (- (vec-x v1) (vec-x v2))
-            (- (vec-y v1) (vec-y v2))
-            (- (vec-z v1) (vec-z v2))))
+(define diff
+  (lambda v
+    (let loop ((v1 (car v))
+               (vecs (cdr v)))
+      (if (null? vecs)
+          v1
+          (loop (vec3 (- (x v1) (x (car vecs)))
+                      (- (y v1) (y (car vecs)))
+                      (- (z v1) (z (car vecs))))
+                (cdr vecs))))))
 
-(define (prod v1 v2)
-  (make-vec (* (vec-x v1) (vec-x v2))
-            (* (vec-y v1) (vec-y v2))
-            (* (vec-z v1) (vec-z v2))))
+(define prod
+  (lambda v
+    (reduce-right (lambda (v1 v2)
+              (vec3 (* (x v1) (x v2))
+                    (* (y v1) (y v2))
+                    (* (z v1) (z v2))))
+            (vec3 1 1 1)
+            v)))
 
-(define (quot v1 v2)
-  (make-vec (/ (vec-x v1) (vec-x v2))
-            (/ (vec-y v1) (vec-y v2))
-            (/ (vec-z v1) (vec-z v2))))
+(define (scale v k)
+  (vec3 (* (x v) k)
+        (* (y v) k)
+        (* (z v) k)))
+
+(define quot
+  (lambda v
+    (let loop ((v1 (car v))
+               (vecs (cdr v)))
+      (if (null? vecs)
+          v1
+          (loop (vec3 (/ (x v1) (x (car vecs)))
+                      (/ (y v1) (y (car vecs)))
+                      (/ (z v1) (z (car vecs))))
+                (cdr vecs))))))
 
 (define (length v)
-  (sqrt (+ (* (vec-x v) (vec-x v))
-           (* (vec-y v) (vec-y v))
-           (* (vec-z v) (vec-z v)))))
+  (sqrt (+ (* (x v) (x v))
+           (* (y v) (y v))
+           (* (z v) (z v)))))
 
 (define (sq-len v)
-  (+ (* (vec-x v) (vec-x v))
-     (* (vec-y v) (vec-y v))
-     (* (vec-z v) (vec-z v))))
+  (+ (* (x v) (x v))
+     (* (y v) (y v))
+     (* (z v) (z v))))
 
 (define (unit v)
   (let ((k (/ 1 (length v))))
     (prod v k)))
 
 (define (dot v1 v2)
-  (+ (* (vec-x v1) (vec-x v2))
-     (* (vec-y v1) (vec-y v2))
-     (* (vec-z v1) (vec-z v2))))
+  (+ (* (x v1) (x v2))
+     (* (y v1) (y v2))
+     (* (z v1) (z v2))))
 
 (define (cross v1 v2)
-  (make-vec (- (* (vec-y v1) (vec-z v2))
-               (* (vec-y v2) (vec-z v1)))
-            (- (* (vec-z v1) (vec-x v2))
-               (* (vec-z v2) (vec-x v1)))
-            (- (* (vec-x v1) (vec-y v2))
-               (* (vec-x v2) (vec-y v1)))))
+  (vec3 (- (* (y v1) (z v2))
+           (* (y v2) (z v1)))
+        (- (* (z v1) (x v2))
+           (* (z v2) (x v1)))
+        (- (* (x v1) (y v2))
+           (* (x v2) (y v1)))))
