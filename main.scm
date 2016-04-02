9bb14bad23858a89f5613f10b03fb7e40da35d82
soma_arc <somaarcr@gmail.com>
2016-04-02 15:17:07 +0900

    Use vector for data structure for objects

--- main.scm
+++ main.scm
@@ -19,10 +19,10 @@
   (let ((n 300)
         (obj-list '()))
     (push! obj-list
-           (make g:<sphere>
-             :center (v:vec3 0 -1000 0) :radius 1000
-             :material (make m:<lambertian>
-                         :albedo (v:vec3 0.5 0.5 0.5))))
+           (g:make-sphere
+            (v:vec3 0 -1000 0) 1000
+            (make m:<lambertian>
+              :albedo (v:vec3 0.5 0.5 0.5))))
     (let loop-a ((a -1))
       (if (< a 1)
           (let loop-b ((b -1))
@@ -35,44 +35,39 @@
                       (cond
                        ((< choose-mat 0.8)
                         (push! obj-list
-                               (make g:<sphere>
-                                 :center center :radius 0.2
-                                 :material (make m:<lambertian>
-                                             :albedo (v:vec3 (* (random-real) (random-real))
-                                                             (* (random-real) (random-real))
-                                                             (* (random-real) (random-real)))))))
+                               (g:make-sphere
+                                center 0.2
+                                (make m:<lambertian>
+                                  :albedo (v:vec3 (* (random-real) (random-real))
+                                                  (* (random-real) (random-real))
+                                                  (* (random-real) (random-real)))))))
                        ((< choose-mat 0.95)
                         (push! obj-list
-                               (make g:<sphere>
-                                 :center center :radius 0.2
-                                 :material (make m:<metal>
-                                             :albedo (v:vec3 (* 0.5 (+ 1 (random-real)))
-                                                             (* 0.5 (+ 1 (random-real)))
-                                                             (* 0.5 (+ 1 (random-real))))
-                                             :fuzz (* 0.5 (random-real))))))
+                               (g:make-sphere
+                                center 0.2
+                                (make m:<metal>
+                                  :albedo (v:vec3 (* 0.5 (+ 1 (random-real)))
+                                                  (* 0.5 (+ 1 (random-real)))
+                                                  (* 0.5 (+ 1 (random-real))))
+                                  :fuzz (* 0.5 (random-real))))))
                        (else (push! obj-list
-                                    (make g:<sphere>
-                                      :center center :radius 0.2
-                                      :material (make m:<dielectric>
-                                                  :ref-idx 1.5))))))
+                                    (g:make-sphere
+                                     center 0.2
+                                     (make m:<dielectric> :ref-idx 1.5))))))
                   (loop-b (inc! b)))
                 (loop-a (inc! a))))))
     (push! obj-list
-           (make g:<sphere>
-             :center (v:vec3 0 1 0) :radius 11
-             :material (make m:<dielectric>
-                         :ref-idx 1.5)))
+           (g:make-sphere
+            (v:vec3 0 1 0) 11
+            (make m:<dielectric> :ref-idx 1.5)))
     (push! obj-list
-           (make g:<sphere>
-             :center (v:vec3 -4 1 0) :radius 1
-             :material (make m:<lambertian>
-                         :albedo (v:vec3 0.4 0.2 0.1))))
+           (g:make-sphere
+            (v:vec3 -4 1 0) 1
+            (make m:<lambertian> :albedo (v:vec3 0.4 0.2 0.1))))
     (push! obj-list
-           (make g:<sphere>
-             :center (v:vec3 4 1 0) :radius 1
-             :material (make m:<metal>
-                         :albedo (v:vec3 0.7 0.6 0.5)
-                         :fuzz 0)))))
+           (g:make-sphere
+            (v:vec3 4 1 0) 1
+            (make m:<metal> :albedo (v:vec3 0.7 0.6 0.5) :fuzz 0)))))
 
 (define (color r obj-list depth)
   (if (> depth +max-depth+)
@@ -91,70 +86,61 @@
                             (v:scale (v:vec3 0.5 0.7 1.0) t)))))))
 
 
-(let* ((nx 200)
-       (ny 100)
-       (ns 10)
-       (lookfrom (v:vec3 3 3 2))
-       (lookat (v:vec3 0 0 -1))
-       (aperture 2.0)
-       (dist-to-focus (v:length (v:diff lookfrom lookat)))
-       (camera (cam-with-lens lookfrom
-                              lookat
-                              (v:vec3 0 1 0)
-                              20 (/ nx ny)
-                              aperture
-                              dist-to-focus))
-       (R (cos pi/4))
-       (obj-list
-        (list (make g:<sphere>
-                :center (v:vec3 0 0 -1)
-                :radius 0.5
-                :material (make m:<lambertian>
-                            :albedo (v:vec3 0.1 0.2 0.5)))
-              (make g:<sphere>
-                :center (v:vec3 0 -100.5 -1)
-                :radius 100
-                :material (make m:<lambertian>
-                            :albedo (v:vec3 0.8 0.8 0)))
-              (make g:<sphere>
-                :center (v:vec3 1 0 -1)
-                :radius 0.5
-                :material (make m:<metal>
-                            :albedo (v:vec3 0.8 0.6 0.2)
-                            :fuzz 1.0))
-              (make g:<sphere>
-                :center (v:vec3 -1 0 -1)
-                :radius 0.5
-                :material (make m:<dielectric>
-                            :ref-idx 1.5))
-              (make g:<sphere>
-                :center (v:vec3 -1 0 -1)
-                :radius -0.45
-                :material (make m:<dielectric>
-                            :ref-idx 1.5)))))
-  (with-output-to-file "test.ppm"
-    (lambda ()
-      (display (format "P3\n ~D ~D\n255\n" nx ny))
-      (let loop-y ((y (- ny 1)))
-        (if (>= y 0)
-            (let loop-x ((x 0))
-              (if (< x nx)
-                  (let* ((c (let loop ((sample-count 0)
-                                       (col (v:vec3 0 0 0)))
-                              (if (>= sample-count ns)
-                                  (v:quot col (v:vec3 ns ns ns))
-                                  (let* ((u (/ (+ x (random-real)) nx))
-                                         (v (/ (+ y (random-real)) ny))
-                                         (ray (get-ray-with-lens camera u v))
-                                         (col (v:sum col
-                                                     (color ray obj-list 0))))
-                                    (loop (+ 1 sample-count) col)))))
-                         (c (v:vec3 (sqrt (v:x c)) (sqrt (v:y c)) (sqrt (v:z c))))
-                         (ir (floor->exact (* 255.99 (v:x c))))
-                         (ig (floor->exact (* 255.99 (v:y c))))
-                         (ib (floor->exact (* 255.99 (v:z c)))))
-                    (display (format "~D ~D ~D\n"
-                                     ir ig ib))
-                    (loop-x (+ x 1)))
-                  (loop-y (- y 1)))))))))
+(time (let* ((nx 200)
+             (ny 100)
+             (ns 10)
+             (lookfrom (v:vec3 3 3 2))
+             (lookat (v:vec3 0 0 -1))
+             (aperture 1.0)
+             (dist-to-focus (v:length (v:diff lookfrom lookat)))
+             (camera (cam lookfrom
+                          lookat
+                          (v:vec3 0 1 0)
+                          45 (/ nx ny)
+;                          aperture
+;                          dist-to-focus
+                          ))
+             (R (cos pi/4))
+             (obj-list
+              (list (g:make-sphere (v:vec3 0 0 -1) 0.5
+                                   (make m:<lambertian>
+                                     :albedo (v:vec3 0.1 0.2 0.5)))
+                    (g:make-sphere (v:vec3 0 -100.5 -1) 100
+                                   (make m:<lambertian>
+                                     :albedo (v:vec3 0.8 0.8 0)))
+                    (g:make-sphere (v:vec3 1 0 -1) 0.5
+                                   (make m:<metal>
+                                     :albedo (v:vec3 0.8 0.6 0.2)
+                                     :fuzz 0.3))
+                    (g:make-sphere (v:vec3 -1 0 -1) 0.5
+                                   (make m:<dielectric>
+                                     :ref-idx 1.5))
+                    (g:make-sphere (v:vec3 -1 0 -1) -0.45
+                                   (make m:<dielectric>
+                                     :ref-idx 1.5)))))
+        (with-output-to-file "test.ppm"
+          (lambda ()
+            (display (format "P3\n ~D ~D\n255\n" nx ny))
+            (let loop-y ((y (- ny 1)))
+              (if (>= y 0)
+                  (let loop-x ((x 0))
+                    (if (< x nx)
+                        (let* ((c (let loop ((sample-count 0)
+                                             (col (v:vec3 0 0 0)))
+                                    (if (>= sample-count ns)
+                                        (v:quot col (v:vec3 ns ns ns))
+                                        (let* ((u (/ (+ x (random-real)) nx))
+                                               (v (/ (+ y (random-real)) ny))
+                                               (ray (get-ray camera u v))
+                                               (col (v:sum col
+                                                           (color ray obj-list 0))))
+                                          (loop (+ 1 sample-count) col)))))
+                               (c (v:vec3 (sqrt (v:x c)) (sqrt (v:y c)) (sqrt (v:z c))))
+                               (ir (floor->exact (* 255.99 (v:x c))))
+                               (ig (floor->exact (* 255.99 (v:y c))))
+                               (ib (floor->exact (* 255.99 (v:z c)))))
+                          (display (format "~D ~D ~D\n"
+                                           ir ig ib))
+                          (loop-x (+ x 1)))
+                        (loop-y (- y 1))))))))))
 
